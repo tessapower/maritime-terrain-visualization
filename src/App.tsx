4ff026ff2fead8088beced17da22184e4ea7cba0
Tessa Power <hello@tessapower.xyz>
2025-10-11 13:25:00 -0500

    Add console component to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,25 @@
 import "./App.css";
+import { useState, useEffect } from "react";
+import { Console } from "./components/gui/Console";
+import { logger } from "./three/utils/Logger";
 import { Scene } from "./components/Scene";
 
 function App() {
+  const [logs, setLogs] = useState<string[]>([]);
+
+  useEffect(() => {
+    // Subscribe to Logger updates
+    const unsubscribe = logger.subscribe(setLogs);
+
+    // Initial messages
+    logger.log("INITIALIZING SYSTEM...");
+    return unsubscribe;
+  }, []);
+
   return (
     <div style={{ width: "100vw", height: "100vh", margin: 0, padding: 0 }}>
       <Scene />
-      {/* TODO: Add UI Overlays */}
+      <Console messages={logs} />
     </div>
   );
 }
